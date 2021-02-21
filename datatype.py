cf1ff8dde5659e70a0d8e316c5a93527a91938ce
Lis <lisjardim6@gmail.com>
2021-02-20 23:29:28 -0300

    Python Data Types

---
+++ datatype.py
@@ -0,0 +1,26 @@
+# Strings
+print("Hello World")
+print("""Hello World""")
+
+# Integer
+print(10)
+
+# Float
+print(30.1)
+
+# Boolean
+True, False
+
+# List --> Se puede cambiar 
+[10, 20, 30, 55]
+
+# Tuples --> no se puede cambiar, inmutable
+(10, 20, 30, 55)
+
+# Dictionaries
+print(type({"name": "Ryan",
+"lastaname": "Ray",
+"surname": "Fazt"}))
+
+# None Sin tipo definido
+
